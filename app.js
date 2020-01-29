dc8d63386427e1327f14e6e08a880edf793214ac
nathan <ahrl_ahrl@naver.com>
2020-01-29 19:56:53 +0900

    이벤트추가 수정,삭제 리팩토링 전

--- app.js
+++ app.js
@@ -3,102 +3,91 @@ const contentsList = contents.querySelectorAll("div");
 const startBtn = document.querySelector(".startBtn");
 const realLife = document.querySelector(".realLife");
 const realMessage = document.querySelector(".realMessage");
-// console.log(contentsList);
 
 const imgKeyStorage = {};
 let clearImgCount = 0;
 let beforeImgKey;
-// 인덱스, 키
 let life;
 
-// function createNode(key) {
-//     const node = {};
+function endGame(hasEnding) {
+    if (hasEnding) {
+        alert("GAME CLEAR !");
+    } else {
+        alert("GMAE OVER");
+        imgEventRemove(Object.keys(imgKeyStorage));
+    }
 
-//     node.key = key;
-//     node.hasComplete = null;
-
-//     return node;
-// }
-
-function endGame() {
-    startBtn.addEventListener("click", startGame);
-    // 게임시작 버튼 누르게 만들어줘야함
-    imgEventHandler(false);
     imgViewHandler();
+    startBtn.addEventListener("click", startGame);
+    // console.log(imgKeyStorage);
+    // imgEveryEventRemove();
     // 초기값 셋팅  => 모든 사진0, 이벤트 제거
-    
 }
 
 
 function matchingImg(event) {
     console.log("이미지 크릭 이벤트 실행");
     let currentTargetIndex;
-    //contentsList.indexOf(event.target.parentNode)
-    // 위 실행 안됨, 유사배열 indexOf 안되는지 ?
 
-    // 클릭한 div가 컨텐츠의 몇번째 요소인지 인덱스 찾기
     for (let i = 0; i < contentsList.length; i++) {
         if (contentsList[i] === event.target.parentNode) {
             currentTargetIndex = i;
-            // +1 이유는 키저장소 키값은 1부터시작이라서
         }
     }
-    // console.log(currentTargetIndex);
 
     if (beforeImgKey) {
-        console.log("이전 이미지 있다", beforeImgKey);
+          console.log("이전 이미지 있다", beforeImgKey);
 
         if (imgKeyStorage[currentTargetIndex + 1] === beforeImgKey[1]) {
             clearImgCount++;
-            //이전키 , 현재 누른 이미지 키 같으면
+            //비포키 , 현재 누른 이미지 키 같으면
             contentsList[currentTargetIndex].firstChild.removeEventListener("click", matchingImg);
             // 현재 이미지의 이벤트 지움
             contentsList[currentTargetIndex].firstChild.src = `img/${imgKeyStorage[currentTargetIndex + 1]}.png`;
             //현재 키 화면에 보여주기
-            
+            delete imgKeyStorage[beforeImgKey[0]];
+            delete imgKeyStorage[currentTargetIndex + 1];
+            //맞췃으니 키삭제
+
             beforeImgKey = null;
             //비포 이미지 키 비우고
-            console.log("이전 이미지 키, 비포키 같다", life, clearImgCount);
+            console.log("두 값 같다", beforeImgKey, "맞춘숫자", clearImgCount);
 
             if (clearImgCount === 6) {
-                endGame();
+            console.log("6개 모두 맞춤", "맞춘숫자", clearImgCount);
+                endGame(true);
+                return;
             }
 
         } else {
-            
-            //이미지 있지만, 비포 키값과 다르다
+            //이미지 있지만, 비포 키값과 다르다 => 못맞춤
             life -= 1;
             realLife.innerText = life;
 
             console.log("이전 이미지와 다르다", life, clearImgCount);
 
             contentsList[currentTargetIndex].firstChild.src = `img/${imgKeyStorage[currentTargetIndex + 1]}.png`;
-            contentsList[currentTargetIndex].firstChild.removeEventListener("click", matchingImg);
-            imgEventHandler(false);
-            //이벤트제거 , 해당키 이미지 보여주기
-
+            // contentsList[currentTargetIndex].firstChild.removeEventListener("click", matchingImg);
+            imgEventRemove(Object.keys(imgKeyStorage));
+            //이벤트제거 모든 img요소 X 키값들만
+            
             if (!life) {
-                alert("겜끝");
-                endGame();
+                endGame(false);
+                return;
             }
-
+            
             setTimeout(() => {
-                //비포 이미지, 현재이미지 이벤트 추가해주고, 
-
+                //3초후 비포키, 선택이미지 모두 이벤트 추가, 
                 // 이미지 0
-                //비포이미지 인덱스도 있어야 그림 맞추기 틀리면 1번쨰 인덱스 사진0 바꾸고
-
-                // 비포인덱스 널
-                contentsList[beforeImgKey[0]].firstChild.src = `img/${0}.png`;
-                contentsList[beforeImgKey[0]].addEventListener("click", matchingImg);
-
-                contentsList[currentTargetIndex].firstChild.src = `img/${0}.png`;
-                contentsList[currentTargetIndex].addEventListener("click", matchingImg);
                 
+                contentsList[beforeImgKey[0]].firstChild.src = `img/${0}.png`;
+                contentsList[currentTargetIndex].firstChild.src = `img/${0}.png`;
                 beforeImgKey = null;
-
-            }, 3000);
-            // 누른것 사진 키값이 3초동안 나오고, 2개 사진 모두 0으로 만들어줘야함
+                imgEventAdd(Object.keys(imgKeyStorage));
+                // contentsList[beforeImgKey[0]].addEventListener("click", matchingImg);
+                // contentsList[currentTargetIndex].addEventListener("click", matchingImg);
+                
+            }, 2000);
             console.log("이전 이미지와 키값 다름", life);
             
             // 게임끝
@@ -107,27 +96,33 @@ function matchingImg(event) {
 
     } else {
         // 이전 이미지가 없으면
-        console.log("이전 이미지 없다 비포키 담김", beforeImgKey);
         beforeImgKey = [currentTargetIndex, imgKeyStorage[currentTargetIndex + 1]];
+        console.log("이전 이미지 없다 비포키 담김", beforeImgKey);
         //비포키에 키저장소의 현재 인덱스 키값 들어감 => 이미지생성 당시 이미지 번호 
         event.target.removeEventListener("click", matchingImg);
-        //이벤트 제거
+        // 처음 누른 사진 => 이벤트 제거
         event.target.src = `img/${imgKeyStorage[currentTargetIndex + 1]}.png`;
-        //현재 인덱스 이미지로 변경
+        //현재 누른사진 => 인덱스 이미지로 변경
 
     }
 }
 
-function imgEventHandler(hasEventCheck) {
-    if (hasEventCheck) {
-        contentsList.forEach(ele => {
-            ele.firstChild.addEventListener("click", matchingImg);
-        });
-    } else {
-        contentsList.forEach(ele => {
-            ele.firstChild.removeEventListener("click", matchingImg);
-        });
-    }
+// function imgEveryEventRemove() {
+//     contesntsList.forEach(item => {
+//         item.firstChild.removeEventListener("click", matchingImg);
+//     });
+// }
+
+function imgEventRemove(everyItem) {
+    everyItem.forEach(ele => {
+        contentsList[ele - 1].firstChild.removeEventListener("click", matchingImg);
+    });
+}
+
+function imgEventAdd(everyItem) {
+    everyItem.forEach(ele => {
+        contentsList[ele - 1].firstChild.addEventListener("click", matchingImg);
+    });
 }
 
 // 이미지 12개 보여주고, 3초후 내리기
@@ -153,21 +148,15 @@ function imgViewHandler(randomNumArr) {
 
 
 
-function viewEveryImg() {
-    // randomNumArr.forEach((key, index) => {
-    //     contentsList[index].firstChild.src = `img/${key}.png`;
-    //     // imgKeyStorage[index + 1] = createNode(key);
-    //     imgKeyStorage[index + 1] = key;
-    // });
-
+function initSeting() {
     for (let i = 1; i <= 3; i++) {
         setTimeout(() => {
             console.log(i);
             if (i === 3) {
-                contentsList.forEach( ele => {
-                    ele.firstChild.src = `img/${0}.png`;
-                });
-                imgEventHandler(true);
+                imgViewHandler();
+                // 모든 사진 0
+                imgEventAdd(Object.keys(imgKeyStorage));
+                //모든 이미지 이벤트주기
             }
 
         }, i * 1000);
@@ -206,13 +195,15 @@ function selectRandomNum() {
 }
 
 function startGame() {
-    life = 3;
-    realLife.innerText = life;
     const randomNumArr = selectRandomNum();
 
+    life = 3;
+    realLife.innerText = life;
     startBtn.removeEventListener("click", startGame);
+
     imgViewHandler(randomNumArr);
-    viewEveryImg();
+    //FIXME: 전체 다른 이미지 보여주거나 , 0 바꾸기 함수 2개로 나누기
+    initSeting();
 }
 
 function init() {
